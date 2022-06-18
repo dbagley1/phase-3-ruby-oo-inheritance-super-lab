fe20c2c08fd3bf024614649f6591e1bc05c020a9
Darrian Bagley <dbagle14@gmail.com>
2022-06-18 13:35:39 -0400

    Completed assignment

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,15 +1,15 @@
-describe Student do 
+describe Student do
   let!(:student) { Student.new }
-  
-  describe "#hello" do 
-    it "returns a greeting phrase" do 
+
+  describe "#hello" do
+    it "returns a greeting phrase" do
       expected_output = "Hey there! I'm so excited to learn stuff.\n"
       expect { student.hello }.to output(expected_output).to_stdout
     end
   end
 
-  describe "#raise_hand" do 
-    it "returns the phrase 'Pick me!'" do 
+  describe "#raise_hand" do
+    it "returns the phrase 'Pick me!'" do
       expected_output = "Pick me!\n"
       expect { student.raise_hand }.to output(expected_output).to_stdout
     end
